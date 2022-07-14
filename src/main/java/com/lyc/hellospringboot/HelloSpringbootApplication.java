1e0f5c1f92f1f549b2e9ec23c83a6ff50373f38c
LiCarry666 <64950424+LiCarry666@users.noreply.github.com>
2022-07-14 22:32:17 +0800

    Update HelloSpringbootApplication.java

--- src/main/java/com/lyc/hellospringboot/HelloSpringbootApplication.java
+++ src/main/java/com/lyc/hellospringboot/HelloSpringbootApplication.java
@@ -15,6 +15,7 @@ public class HelloSpringbootApplication {
         SpringApplication.run(HelloSpringbootApplication.class, args);
         System.out.println("springboot running");
         System.out.println("springboot runnable");
+        System.out.println("springboot success");
     }
 
     @GetMapping("/hello")
